aeb1fafc656dcc95d931fa9f29a872b7a5e5ed36
Fardin Ahmed Alif <Alif.Ahmed44@gmail.com>
2024-09-01 23:51:57 +0600

    Jwt, middlewares, and DB_Collections added

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
-require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-// MIDDLEWARE
-app.use(cors());
+// **MIDDLEWARE
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+    optionSuccessStatus: 200,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
+// ** jwt middleware
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized Access" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "Unauthorized Access" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
 
-// ---------------------------------------------------
-
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.plfcipz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -24,26 +43,66 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    console.log("You successfully connected to MongoDB!");
+
+    // **JWT added
+    const cookieOption = {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    };
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.cookie("token", token, cookieOption);
+      res.send({ Success: true });
+    });
+    app.post("/logout", async (req, res) => {
+      const user = req.body;
+      res.clearCookie("token", { ...cookieOption, maxAge: 0 });
+      res.send({ success: true });
+    });
+    // **JWT End
+    // ** DataBase Collections :
+    const userCollection = client.db("PRB9-A12").collection("users");
+    const bookedCollection = client.db("PRB9-A12").collection("booked");
+
+    //  ** verify Admin & DeliveryMan middleware
+    const verifyAdmin = async (req, res, next) => {
+      const user = req.user;
+      const query = { email: user?.email };
+      // todo: Add Correct Collection name here
+      const result = await "collectionName".findOne(query);
+      if (!result || result?.role !== "admin")
+        return res.status(401).send({ message: "unauthorized access!!" });
+
+      next();
+    };
+    const verifydeliveryMan = async (req, res, next) => {
+      const user = req.user;
+      const query = { email: user?.email };
+      // todo: Add Correct Collection name here
+      const result = await "collectionName".findOne(query);
+      if (!result || result?.role !== "deliveryMan") {
+        return res.status(401).send({ message: "unauthorized access!!" });
+      }
+      next();
+    };
+
+    // ** All CRUD Operated --
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
+
 run().catch(console.dir);
-
-// ---------------------------------------------------
-
 app.get("/", (req, res) => {
-  res.send("Book is ready to Parcel");
+  res.send("Parcel Management is running");
 });
-
 app.listen(port, () => {
-  console.log(`Book is ready to Parcel On Port ${port}`);
+  console.log(`Parcel Management is running On Port ${port}`);
 });
